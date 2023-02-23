df5490453cbe7fefce1162a625cba40ccdc2a0cb
PolinaAndreeva13 <polya3192@yandex.ru>
2023-02-23 17:18:32 +0700

    Создание отдельного файла с функцией принт

--- Практика1/Boats.js
+++ Практика1/Boats.js
@@ -1,34 +1,3 @@
-/*function Print(){
-
-    display.innerHTML = " ";
-
-    display.innerHTML += "<p></p> "
-
-    for(let i = 65; i <= 75; i++){
-
-        if (i == 73){
-
-            i += 1;
-
-        }
-
-        let ch = String(i);
-
-        display.innerHTML += String.fromCharCode(ch);
-    }
-
-    for(let i = 0; i < myPole.length; i++){
-
-        display.innerHTML += "<p></p> " + (i + 1);
-
-        for(let j = 0; j < myPole[i].length; j++){
-
-            display.innerHTML += myPole[i][j] + ' ';
-        }
-    }
-
-}*/
-
 function Boat(){
 
     if (myPole[1][0] == 'x'){
